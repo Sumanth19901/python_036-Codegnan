ee8268cc99e0eb66059d1632015bd96bf92e4222
Sumanth19901 <kasisumanth8@gmail.com>
2026-06-24 10:46:20 +0530

    update the bank balance file

--- balance.py
+++ balance.py
@@ -11,7 +11,22 @@ def  Deposite(money):
 def  account_details()
 
 
-while :
+while: True:
+    print("1. Check Balance")
+    print("2. Deposite Money")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+    
+    if choice == '1':
+        check_balance()
+    elif choice == '2':
+        amount = float(input("Enter amount to deposite: "))
+        Deposite(amount)
+    elif choice == '3':
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
     
 
     
